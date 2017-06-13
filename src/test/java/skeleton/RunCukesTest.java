4ff4e951ee50916eeb3fd0ffd5a2c0fb7fef3549
xiaozi0lei <xiaozi0lei@163.com>
2017-06-13 19:33:29 +0800

    cucumber json report and logback INFO level

--- src/test/java/skeleton/RunCukesTest.java
+++ src/test/java/skeleton/RunCukesTest.java
@@ -5,7 +5,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"})
+@CucumberOptions(plugin = {"pretty"}, format = {"json:target/cucumber.json"})
 public class RunCukesTest {
 
 }
